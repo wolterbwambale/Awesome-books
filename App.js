13e13ec929927f99571d7034343570af04344d9c
innocent259 <ndahiminnocent@gmail.com>
2023-03-27 21:55:00 +0200

    Add JavaScript part

--- App.js
+++ App.js
@@ -1,4 +1,44 @@
-const books = [{
-    title : 'title name',
-    author : 'author name'
-}];
+function Book(title, author) {
+    this.title = title;
+    this.author = author;
+  }
+  
+  let booksCollection = []; 
+  function removeBookFromCollection(title, author) {
+    booksCollection = booksCollection.filter((book) => book.title !== title || book.author !== author);
+  }
+  
+  function displayBooks() {
+    const booksContainer = document.getElementById('book-list');
+    booksContainer.innerHTML = '';
+  
+    for (let i = 0; i < booksCollection.length; i += 1) {
+      const book = booksCollection[i];
+      const bookElement = document.createElement('div');
+      bookElement.innerHTML = `<p>Title: ${book.title}</p><p>Author: ${book.author}</p>`;
+  
+      const removeButton = document.createElement('button');
+      removeButton.textContent = 'Remove';
+      removeButton.addEventListener('click', () => {
+        removeBookFromCollection(book.title, book.author);
+        displayBooks();
+      });
+      bookElement.appendChild(removeButton);
+      booksContainer.appendChild(bookElement);
+    }
+  }
+  
+  function addBookToCollection(title, author) {
+    const newBook = new Book(title, author);
+    booksCollection.push(newBook);
+    displayBooks();
+  }
+  const addButton = document.getElementById('add-button');
+  addButton.addEventListener('click', (event) => {
+    event.preventDefault(); 
+    const titleInput = document.getElementById('title');
+    const authorInput = document.getElementById('author');
+    addBookToCollection(titleInput.value, authorInput.value);
+    titleInput.value = ''; 
+    authorInput.value = ''; 
+  });
