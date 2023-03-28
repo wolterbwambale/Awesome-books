58f74f047154e6371caeaa4ca307c0481cfa24b5
innocent259 <ndahiminnocent@gmail.com>
2023-03-28 17:33:54 +0200

    Declare class

--- App.js
+++ App.js
@@ -1,60 +1,7 @@
-function loadFromLocalStorage() {
-  return JSON.parse(localStorage.getItem('booksCollection'));
-}
-let booksCollection = loadFromLocalStorage() || [];
-
-function saveToLocalStorage() {
-  localStorage.setItem('booksCollection', JSON.stringify(booksCollection));
-}
-
-function Book(title, author) {
-  this.title = title;
-  this.author = author;
-}
-
-function removeBookFromCollection(title, author) {
-  booksCollection = booksCollection.filter(
-    (book) => book.title !== title || book.author !== author,
-  );
-  saveToLocalStorage();
-}
-
-function displayBooks() {
-  const booksContainer = document.getElementById('book-list');
-  booksContainer.innerHTML = '';
-
-  for (let i = 0; i < booksCollection.length; i += 1) {
-    const book = booksCollection[i];
-    const bookElement = document.createElement('div');
-    bookElement.innerHTML = `<p>Title: ${book.title}</p>
-                             <p>Author: ${book.author}</p>`;
-
-    const removeButton = document.createElement('button');
-    removeButton.textContent = 'Remove';
-    removeButton.addEventListener('click', () => {
-      removeBookFromCollection(book.title, book.author);
-      displayBooks();
-    });
-    bookElement.appendChild(removeButton);
-    booksContainer.appendChild(bookElement);
+class Book {
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
   }
+  //Method for adding book
 }
-
-function addBookToCollection(title, author) {
-  const newBook = new Book(title, author);
-  booksCollection.push(newBook);
-  saveToLocalStorage();
-  displayBooks();
-}
-
-const addButton = document.getElementById('add-button');
-addButton.addEventListener('click', (event) => {
-  event.preventDefault();
-  const titleInput = document.getElementById('title');
-  const authorInput = document.getElementById('author');
-  addBookToCollection(titleInput.value, authorInput.value);
-  titleInput.value = '';
-  authorInput.value = '';
-});
-
-displayBooks();
