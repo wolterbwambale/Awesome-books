47a66510bbc28e5a5971a3d65b49baf1a0a5f9c3
wolterbwambale <benplaan1@gmail.com>
2023-03-29 11:18:11 +0300

    update App.js

--- App.js
+++ App.js
@@ -3,8 +3,67 @@ class Book {
     this.title = title;
     this.author = author;
   }
-  //Method for adding book
-  addBook () {
-    const newBook = new Book(title, author)
-  }
 }
+
+
+class BookCollection {
+  constructor() {
+    this.books =  [];
+    this.container = document.getElementById('book-list');
+    this.addButton = document.getElementById('add-button');
+    this.titleInput = document.getElementById('title');
+    this.authorInput = document.getElementById('author');
+    
+    this.addButton.addEventListener('click', (event) => {
+      event.preventDefault(); 
+      this.addBookToCollection(this.titleInput.value, this.authorInput.value);
+      this.titleInput.value = ''; 
+      this.authorInput.value = ''; 
+    });
+    
+    this.displayBooks();
+  }
+  
+  addBookToCollection(title, author) {
+    const newBook = new Book(title, author);
+    this.books.push(newBook);
+    // this.saveToLocalStorage();
+    this.displayBooks();
+  }
+  
+  removeBookFromCollection(title, author) {
+    this.books = this.books.filter((book) => book.title !== title || book.author !== author);
+    // this.saveToLocalStorage();
+    this.displayBooks();
+  }
+  
+  displayBooks() {
+    this.container.innerHTML = '';
+  
+    for (let i = 0; i < this.books.length; i += 1) {
+      const book = this.books[i];
+      const bookElement = document.createElement('div');
+      bookElement.innerHTML = `<p>Title: ${book.title}</p>
+                               <p>Author: ${book.author}</p>`;
+  
+      const removeButton = document.createElement('button');
+      removeButton.textContent = 'Remove';
+      removeButton.addEventListener('click', () => {
+        this.removeBookFromCollection(book.title, book.author);
+      });
+      
+      bookElement.appendChild(removeButton);
+      this.container.appendChild(bookElement);
+    }
+  }
+  
+  // loadFromLocalStorage() {
+  //   return JSON.parse(localStorage.getItem('booksCollection'));
+  // }
+  
+  // saveToLocalStorage() {
+  //   localStorage.setItem('booksCollection', JSON.stringify(this.books));
+  // }
+}
+
+const bookCollection = new BookCollection();
