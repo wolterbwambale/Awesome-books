9c8066a5fc45d09baef1d0c7ffede9860d032200
innocent259 <ndahiminnocent@gmail.com>
2023-03-31 15:36:33 +0200

    Add page functionalities

--- App.js
+++ App.js
@@ -68,3 +68,29 @@ class Library {
 }
 // eslint-disable-next-line no-unused-vars
 const library = new Library();
+
+const headerLinks = document.querySelector("#header");
+const popList = document.querySelector('#pop-list')
+const listOfBooks = document.querySelector('#book-lists')
+const popAdded = document.querySelector('#pop-added')
+const addBook = document.querySelector('.add-book-section')
+const popContact = document.querySelector('#pop-contact')
+const showContact = document.querySelector('.contact')
+
+headerLinks.addEventListener("click", function(e) {
+  e.preventDefault();
+  const currentLink = e.target;
+  if (currentLink === popList) {
+    listOfBooks.classList.add('show-list');
+    addBook.classList.remove('show-add-menu');
+    showContact.classList.remove('show-contact');
+  } else if (currentLink === popAdded) {
+    addBook.classList.add('show-add-menu');
+    listOfBooks.classList.remove('show-list');
+    showContact.classList.remove('show-contact');
+  } else if (currentLink === popContact) {
+    showContact.classList.add('show-contact');
+    listOfBooks.classList.remove('show-list');
+    addBook.classList.remove('show-add-menu');
+  }
+});
